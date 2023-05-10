e60daed56298006ba70454b055cdcf83507b893e
Alberto Montealegre <alberto_mt@hotmail.es>
2023-05-11 00:56:25 +0200

    SimulacionCockies/js

---
+++ JQuery/simulacionCockies/js/simulacionCockies.js
@@ -0,0 +1,25 @@
+if (Cookies.get("aceptaCookies") != "aceptada") {
+  $(document).ready();
+}
+
+$(document).ready(function () {
+  if (Cookies.enabled) {
+    $().lanzarAvisoCookies("Acepta las cookies para seguir navegando");
+  } else {
+    $().lanzarAvisoCookies(
+      "El navegador no Acepta Cookies. Actívalo o la página no funcionará correctamente"
+    );
+  }
+});
+
+$.fn.lanzarAvisoCookies = function (mensaje) {
+  var elementoCookies = document.createElement("div");
+  elementoCookies.innerHTML = "<p>" + mensaje + " <span>Aceptar</span></p>";
+  elementoCookies.classList.add("avisoCookies");
+  document.body.appendChild(elementoCookies);
+
+  $("span").click(function () {
+    Cookies.set("aceptaCookies", "aceptada", { expires: 7776000 }); // Caduca en 3 meses
+    $(".avisoCookies").fadeOut(5000);
+  });
+};
