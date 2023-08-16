851f4af1d78452cf5a5e8ad8642f8438e965e4a6
Alberto Montealegre <alberto_mt@hotmail.es>
2023-08-16 12:05:19 +0200

    modified:   JavaScript/Modo_oscuro_con_session_storage/js/script.js

--- JavaScript/Modo_oscuro_con_session_storage/js/script.js
+++ JavaScript/Modo_oscuro_con_session_storage/js/script.js
@@ -1,26 +1,22 @@
-window.onload = function() {
-    let check_theme = document.getElementById("ckb-theme");
-    check_theme.addEventListener("change", cambiar_tema);
+window.addEventListener("load", () =>{
+  let check_theme = document.getElementById("ckb-theme");
+  check_theme.addEventListener("change", cambiar_tema);
 
-    // Añadir tema
+  // Añadir tema
+  sessionStorage.setItem("theme", "light");
+  document.documentElement.classList.add(sessionStorage.getItem("theme"));
+});
+
+function cambiar_tema() {
+  var element = document.documentElement;
+
+  if (this.checked) {
+    sessionStorage.setItem("theme", "dark");
+    element.classList.replace("light", sessionStorage.getItem("theme"));
+    check_theme.classList.replace("true", "false");
+  } else {
     sessionStorage.setItem("theme", "light");
-    document.documentElement.classList.add(sessionStorage.getItem("theme"));
-
-    /**
-     * cambiar_tema() - método para cambiar estilos de tema de página
-     */
-    function cambiar_tema() {
-        var element = document.documentElement;
-
-        if (this.checked) {
-            sessionStorage.setItem("theme", "dark");
-            element.classList.replace('light',sessionStorage.getItem("theme"));
-            check_theme.classList.replace('true', 'false');
-        }
-        else {
-            sessionStorage.setItem("theme", "light");
-            element.classList.replace('dark',sessionStorage.getItem("theme"));
-            check_theme.classList.replace('false', 'true')
-        }
-    }
+    element.classList.replace("dark", sessionStorage.getItem("theme"));
+    check_theme.classList.replace("false", "true");
+  }
 }
